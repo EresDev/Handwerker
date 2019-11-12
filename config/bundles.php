b295f0d3ff33ab4df8c20ea535355bc8b1b47bec
arslan <arslanafzal321@gmail.com>
2019-11-13 03:01:34 +0500

    Removed Liip, Going to try Alice for fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,4 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
