c4ba6364bb2d8a784b411159c5ea6162b2d3a58f
arslan <arslanafzal321@gmail.com>
2019-12-19 23:59:11 +0500

    Tests for Create Job Validations added.

--- tests/Unit/Application/CommandHandler/CreateJobHandlerTest.php
+++ tests/Unit/Application/CommandHandler/CreateJobHandlerTest.php
@@ -11,6 +11,7 @@ use App\Domain\Exception\ValidationException;
 use App\Domain\Repository\Job\JobSaver;
 use App\Tests\Shared\KernelTestCase;
 use App\Tests\Shared\ObjectMother\JobMother;
+use App\Tests\Shared\TestData;
 
 class CreateJobHandlerTest extends KernelTestCase
 {
@@ -19,10 +20,9 @@ class CreateJobHandlerTest extends KernelTestCase
     private Uuid $uuidGenerator;
     private AssociatedEntityCreator $associatedEntityCreator;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         static::bootKernel();
-        parent::setUp();
 
         $this->validator = $this->getService(Validator::class);
         $this->jobSaver =
@@ -64,26 +64,153 @@ class CreateJobHandlerTest extends KernelTestCase
         );
     }
 
-    public function testHandleWithExecutionDatetimeBeforeAcceptableTime(): void
+    /**
+     * @dataProvider getInvalidValues
+     */
+    public function testHandleForInvalidValues(TestData $testData): void
     {
-        $this->expectException(ValidationException::class);
-        $testData = JobMother::toValidParameterArray(false);
-        $testData['executionDateTime'] = (new \DateTime())->modify('+23 hours');
-
-        $command = $this->getCommandFrom($testData);
+        $command = $this->getCommandFrom($testData->getInput());
 
         $handler = new CreateJobHandler(
             $this->validator,
             $this->jobSaver,
             $this->associatedEntityCreator
         );
+
+        $this->expectException(ValidationException::class);
+
         try {
             $handler->handle($command);
         } catch (ValidationException $exception) {
             $errors = $exception->getMessagesForEndUser();
-            $this->assertArrayHasKey('executionDateTime', $errors[0]);
+            $this->assertArrayHasKey(
+                $testData->getExpectedValue(),
+                $errors[0],
+                $testData->getTestFailureReason()
+            );
+
             $this->assertCount(1, $errors);
             throw $exception;
         }
+    }
+
+    public function getInvalidValues(): array
+    {
+        return [
+            [
+                new TestData(
+                    $this->prepareTestDataForInvalidJob('title', ''),
+                    'title',
+                    'Validation error: ' .
+                    'Given empty title ' .
+                    'but did not get back title validation error'
+                )
+            ],
+            [
+                new TestData(
+                    $this->prepareTestDataForInvalidJob(
+                        'executionDateTime',
+                        $this->getExecutionDateTime23HoursFromNow()
+                    ),
+                    'executionDateTime',
+                    'Validation error: ' .
+                    'Given executionDateTime before 24 hours ' .
+                    'but did not get back executionDateTime validation error'
+                )
+            ],
+            [
+                new TestData(
+                    $this->prepareTestDataForInvalidJob('zipCode', ''),
+                    'zipCode',
+                    'Validation error: ' .
+                    'Given empty zipCode ' .
+                    'but did not get back zipCode validation error'
+                )
+            ],
+            [
+                new TestData(
+                    $this->prepareTestDataForInvalidJob('zipCode', '121212'),
+                    'zipCode',
+                    'Validation error: ' .
+                    'Given 6 chars zipCode ' .
+                    'but did not get back zipCode validation error ' .
+                    'Expected 5 chars zipCode'
+                )
+            ],
+            [
+                new TestData(
+                    $this->prepareTestDataForInvalidJob('zipCode', '1212'),
+                    'zipCode',
+                    'Validation error: ' .
+                    'Given 4 chars zipCode ' .
+                    'but did not get back zipCode validation error ' .
+                    'Expected 5 chars zipCode'
+                )
+            ],
+            [
+                new TestData(
+                    $this->prepareTestDataForInvalidJob('city', ''),
+                    'city',
+                    'Validation error: ' .
+                    'Given empty city ' .
+                    'but did not get back city validation error'
+                )
+            ],
+            [
+                new TestData(
+                    $this->prepareTestDataForInvalidJob('city', 'ab'),
+                    'city',
+                    'Validation error: ' .
+                    'Given too short city ' .
+                    'but did not get back city validation error'
+                )
+            ],
+            [
+                new TestData(
+                    $this->prepareTestDataForInvalidJob(
+                        'city',
+                        str_repeat('a', 51)
+                    ),
+                    'city',
+                    'Validation error: ' .
+                    'Given too long city ' .
+                    'but did not get back city validation error'
+                )
+            ],
+            [
+                new TestData(
+                    $this->prepareTestDataForInvalidJob(
+                        'description',
+                        str_repeat('a', 251)
+                    ),
+                    'description',
+                    'Validation error: ' .
+                    'Given too long description ' .
+                    'but did not get back city validation error'
+                )
+            ],
+            [
+                new TestData(
+                    $this->prepareTestDataForInvalidJob('categoryId', 'fooBar'),
+                    'categoryId',
+                    'Validation error: ' .
+                    'Given invalid format UUID of categoryId ' .
+                    'but did not get back city validation error'
+                )
+            ],
+        ];
+    }
+
+    private function prepareTestDataForInvalidJob(string $attrToOverride, $attrValue): array
+    {
+        $testData = JobMother::toValidParameterArray(false);
+        $testData[$attrToOverride] = $attrValue;
+
+        return $testData;
+    }
+
+    public function getExecutionDateTime23HoursFromNow(): \DateTime
+    {
+        return (new \DateTime())->modify('+23 hours');
     }
 }
