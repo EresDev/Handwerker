22c1857891a98e1eb4eed37ba25f6af3ee2fac43
arslan <arslanafzal321@gmail.com>
2019-12-21 02:54:39 +0500

    Added fixture in PHP to reuse the values.

---
+++ fixtures/category.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Domain\Entity\Category;
+use App\Tests\Shared\Fixture\CategoryFixture;
+
+return [
+    Category::class => [
+        'category_1' => [
+            '__construct' => [
+                CategoryFixture::UUID,
+                CategoryFixture::TITLE,
+                CategoryFixture::IMAGE_URI,
+            ]
+        ],
+    ],
+];
