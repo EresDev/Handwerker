1bc7c00458c56b1672d4fc7d97e9e21110714408
arslan <arslanafzal321@gmail.com>
2019-12-21 02:54:21 +0500

    Added fixture in PHP to reuse the values.

---
+++ tests/Shared/Fixture/CategoryFixture.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Shared\Fixture;
+
+abstract class CategoryFixture
+{
+    public const UUID = '0d195d63-cdc3-4286-90c9-0d6bb8e913ce';
+    public const TITLE = 'Sonstige Umzugsleistungen';
+    public const IMAGE_URI = 'image/uri';
+}
