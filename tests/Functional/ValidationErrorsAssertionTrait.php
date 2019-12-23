403c8bdf78d47ca9f5ae27cb319aef978ab07904
arslan <arslanafzal321@gmail.com>
2019-12-24 02:10:53 +0500

    Improved tests by removing duplication for validation errors assertions with trait

---
+++ tests/Functional/ValidationErrorsAssertionTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use Symfony\Component\HttpFoundation\Response;
+
+trait ValidationErrorsAssertionTrait
+{
+    //TODO: find the undefined methods solution
+    protected function assertForValidationError(string $invalidField, string $expectedError): void
+    {
+        $response = $this->response();
+        $this->assertEquals(422, $response->getStatusCode());
+
+        $content = $response->getContent();
+        $contentObjects = json_decode($content);
+
+        $this->assertCount(
+            1,
+            $contentObjects,
+            sprintf(
+                "Number of received validation errors is not exactly one. The errors received are: %s\n",
+                print_r($contentObjects, true)
+            )
+        );
+
+        $this->assertObjectHasAttribute(
+            $invalidField,
+            $contentObjects[0],
+            sprintf(
+                "Validation errors does not contain error for invalid %s. " .
+                "Errors received are: \n%s",
+                $invalidField,
+                print_r($contentObjects, true)
+            )
+        );
+
+        $this->assertEquals(
+            $expectedError,
+            $contentObjects[0]->$invalidField,
+            sprintf("Validation error received for invalid %s is not as expected.", $invalidField)
+        );
+    }
+}
