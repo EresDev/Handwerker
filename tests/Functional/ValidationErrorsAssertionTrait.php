6751a13ef7ef554dbc9497999f839c9a73c61dcc
arslan <arslanafzal321@gmail.com>
2019-12-24 02:12:51 +0500

    removed unused import.

--- tests/Functional/ValidationErrorsAssertionTrait.php
+++ tests/Functional/ValidationErrorsAssertionTrait.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional;
 
-use Symfony\Component\HttpFoundation\Response;
-
 trait ValidationErrorsAssertionTrait
 {
     //TODO: find the undefined methods solution
