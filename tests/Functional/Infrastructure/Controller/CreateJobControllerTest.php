b143c2b5dc5ac9eea844d191973629b80d853fd4
arslan <arslanafzal321@gmail.com>
2019-12-26 00:49:15 +0500

    Removed duplication in create & update job tests 2/2

--- tests/Functional/Infrastructure/Controller/CreateJobControllerTest.php
+++ tests/Functional/Infrastructure/Controller/CreateJobControllerTest.php
@@ -4,15 +4,10 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\Infrastructure\Controller;
 
-use App\Tests\Functional\ValidationErrorsAssertionTrait;
-use App\Tests\Shared\AuthenticatedWebTestCase;
 use App\Tests\Shared\ObjectMother\JobMother;
 
-class CreateJobControllerTest extends AuthenticatedWebTestCase
+class CreateJobControllerTest extends UpsertJobTestCase
 {
-    use ValidationErrorsAssertionTrait;
-    private const URI = ['en' => 'job', 'de' => 'arbeit'];
-
     /**
      * @dataProvider uriProvider
      */
@@ -22,7 +17,7 @@ class CreateJobControllerTest extends AuthenticatedWebTestCase
 
         $this->sendRequest(
             $uri,
-            JobMother::toValidParameterArray()
+            $this->getJobParameters()
         );
         $response = $this->response();
         $this->assertEquals(201, $response->getStatusCode());
@@ -32,139 +27,13 @@ class CreateJobControllerTest extends AuthenticatedWebTestCase
         $this->assertNotNull($responseObj->uuid);
     }
 
-    public function uriProvider(): array
+    protected function getJobParameters(): array
     {
-        return [
-            'EN URI' => [self::URI['en']],
-            'DE URI' => [self::URI['de']]
-        ];
+        return JobMother::toValidParameterArray();
     }
 
-    /**
-     * @dataProvider unauthenticatedDataProvider
-     */
-    public function testHandleRequestForValidDataUnauthenticated(string $uri, string $expectedError): void
+    protected function getRequestMethod(): string
     {
-        $this->sendRequest(
-            $uri,
-            JobMother::toValidParameterArray()
-        );
-        $response = $this->response();
-        $this->assertEquals(
-            401,
-            $response->getStatusCode(),
-            'Test to get error message for unauthorized access to create a new job failed. ' .
-            'Received wrong HTTP status code.'
-        );
-
-        $responseObj = json_decode($response->getContent());
-        $this->assertEquals(
-            $expectedError,
-            $responseObj->message,
-            'Test to get error message for unauthorized access to create a new job failed. ' .
-            'We did not get back the error message we ' .
-            'were expecting.'
-        );
-    }
-
-    public function unauthenticatedDataProvider(): array
-    {
-        //TODO: add translation for this error message
-        return [
-            'EN: Unauthenticated Error' => [self::URI['en'], 'JWT Token not found'],
-            'DE: Unauthenticated Error' => [self::URI['de'], 'JWT Token not found']
-        ];
-    }
-
-    private function sendRequest(string $uri, array $parameters): void
-    {
-        $this->client->request(
-            'post',
-            '/' . $uri,
-            $parameters,
-            [],
-            []
-        );
-    }
-
-    /**
-     * @dataProvider invalidExecutionDateTimeDataProvider
-     */
-    public function testHandleRequestForInvalidExecutionDateTimeAsEmptyString(
-        string $uri,
-        string $expectedError
-    ): void {
-        $this->authenticateClient();
-
-        $jobParameters = JobMother::toValidParameterArray();
-        $jobParameters['executionDateTime'] = '';
-
-        $this->sendRequest($uri, $jobParameters);
-
-        $this->assertForValidationError('executionDateTime', $expectedError);
-    }
-
-    public function invalidExecutionDateTimeDataProvider(): array
-    {
-        return [
-            'EN: Invalid execution datetime' => [
-                self::URI['en'],
-                'The execution DateTime must be after 24 hours from now.'
-            ],
-            'DE: Invalid execution datetime' => [
-                self::URI['de'],
-                'Die Ausführung von DateTime muss nach 24 Stunden erfolgen.'
-            ]
-        ];
-    }
-
-    /**
-     * @dataProvider invalidExecutionDateTimeDataProvider
-     */
-    public function testHandleRequestForInvalidExecutionDateTimeAsNegativeInteger(
-        string $uri,
-        string $expectedError
-    ): void {
-        $this->authenticateClient();
-        $jobParameters = JobMother::toValidParameterArray();
-        $timestampNow = (new \DateTime())
-            ->modify('+2 days')
-            ->getTimestamp();
-        $jobParameters['executionDateTime'] = -$timestampNow;
-
-        $this->sendRequest($uri, $jobParameters);
-
-        $this->assertForValidationError('executionDateTime', $expectedError);
-    }
-
-    /**
-     * @dataProvider validCategoryUuidThatDoesNotExistDataProvider
-     */
-    public function testHandleRequestWithValidCategoryUuidThatDoesNotExist(
-        string $uri,
-        string $expectedError
-    ): void {
-        $this->authenticateClient();
-
-        $jobParameters = JobMother::toValidParameterArray();
-        $jobParameters['categoryId'] = 'd2b00dae-905b-4c19-9c26-d530874a4910';
-
-        $this->sendRequest($uri, $jobParameters);
-
-        $this->assertForValidationError('categoryId', $expectedError);
-    }
-
-    public function validCategoryUuidThatDoesNotExistDataProvider(): array
-    {
-        return [
-            'EN: Valid Category UUID that does not exist in DB' => [
-                self::URI['en'],
-                'Provided category for new job does not exist.'
-            ],
-            'DE: Valid Category UUID that does not exist in DB' => [
-                self::URI['de'],
-                'Die angegebene Kategorie für einen neuen Job existiert nicht.'
-            ]
-        ];
+        return 'post';
     }
 }
