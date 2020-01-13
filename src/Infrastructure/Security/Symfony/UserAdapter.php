ef01edc726d46db9d3dddd421745f0fb738d9357
arslan <arslanafzal321@gmail.com>
2020-01-14 04:11:16 +0500

    unused methods removed.

--- src/Infrastructure/Security/Symfony/UserAdapter.php
+++ src/Infrastructure/Security/Symfony/UserAdapter.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Infrastructure\Security\Symfony;
 
 use App\Domain\Entity\User;
-use DateTime;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserAdapter implements UserInterface
@@ -17,27 +16,7 @@ class UserAdapter implements UserInterface
         $this->user = $user;
     }
 
-    public function getId() : ?int
-    {
-        return $this->user->getId();
-    }
-
-    public function setId(int $id): void
-    {
-        $this->user->setId($id);
-    }
-
-    public function getEmail() : string
-    {
-        return $this->user->getEmail();
-    }
-
-    public function setEmail(string $email): void
-    {
-        $this->user->setEmail($email);
-    }
-
-    public function getPassword() : ?string
+    public function getPassword(): ?string
     {
         return $this->user->getPassword();
     }
@@ -47,47 +26,12 @@ class UserAdapter implements UserInterface
         $this->user->setPassword($password);
     }
 
-    public function getActivated() : bool
-    {
-        return $this->user->getActivated();
-    }
-
-    public function setActivated(bool $active): void
-    {
-        $this->user->setActivated($active);
-    }
-
-    public function getDeleted() : bool
-    {
-        return $this->user->getDeleted();
-    }
-
-    public function setDeleted(bool $deleted): void
-    {
-        $this->user->setDeleted($deleted);
-    }
-
-    public function getMemberSince() : DateTime
-    {
-        return $this->user->getMemberSince();
-    }
-
-    public function setMemberSince(DateTime $memberSince): void
-    {
-        $this->user->setMemberSince($memberSince);
-    }
-
-    public function getRoles() : array
+    public function getRoles(): array
     {
         return $this->user->getRoles();
     }
 
-    public function setRoles($roles) : void
-    {
-        $this->user->setRoles($roles);
-    }
-
-    public function getUsername() : string
+    public function getUsername(): string
     {
         return $this->user->getEmail();
     }
