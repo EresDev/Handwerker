f6dadf864076ade5cc75fc65c20dbed3b1e602c0
arslan <arslanafzal321@gmail.com>
2020-02-04 04:06:50 +0500

    Translator is not needed in GetJobController

--- src/Infrastructure/Controller/GetJobController.php
+++ src/Infrastructure/Controller/GetJobController.php
@@ -7,7 +7,6 @@ namespace App\Infrastructure\Controller;
 use App\Application\Query\GetJobQuery;
 use App\Application\QueryHandler\GetJobHandler;
 use App\Application\Service\Security\Security;
-use App\Application\Service\Translator;
 use App\Domain\Entity\User;
 use App\Infrastructure\Service\Http\ErrorResponseContent;
 use App\Infrastructure\Service\Http\SuccessResponseContent;
@@ -18,18 +17,15 @@ use Symfony\Component\HttpFoundation\RequestStack;
 class GetJobController
 {
     private Request $request;
-    private Translator $translator;
     private User $user;
     private GetJobHandler $handler;
 
     public function __construct(
         RequestStack $requestStack,
-        Translator $translator,
         Security $security,
         GetJobHandler $handler
     ) {
         $this->request = $requestStack->getCurrentRequest();
-        $this->translator = $translator;
         $this->user = $security->getUser();
         $this->handler = $handler;
     }
